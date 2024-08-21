b2f397d9ef31745fce3302b68c0de9f8bbe8a2b5
Gnornibus <subinrong@outlook.com>
2024-08-22 01:07:21 +0800

    :bug: 初步查询出完整的事件信息

--- src/views/admin/test/SysTest/index.vue
+++ src/views/admin/test/SysTest/index.vue
@@ -19,12 +19,52 @@ import axios from 'axios';
 export default {
     data() {
         return {
+            configData: {
+                id: "1819002266717372442",
+                creUserId: "0",
+                creTime: "2024-08-22 00:12:09",
+                updUserId: "-1",
+                updTime: "2024-08-22 00:15:18",
+                version: 2,
+                deleted: 0,
+                tenantId: null,
+                spreadsheetId: "15nnoeazZ52Q5HKZBP6c9sOgoVZD24p8Qa-HMLLQrNE4",
+                status: "complete_config",
+                statusName: "完成配置执行",
+                remark: null,
+                outputHeader: "TRUE",
+                outputHeaderName: "是",
+                ignoreDrive: "FALSE",
+                ignoreDriveName: "否",
+                sourceUrl: "15nnoeazZ52Q5HKZBP6c9sOgoVZD24p8Qa-HMLLQrNE4",
+                sourceSheet: "PA_ALL",
+                dataRange: "A4:G",
+                judgeCondition: "email!=''",
+                compareField: "email;submitted",
+                outputField: "",
+                outputMode: "first",
+                cron: "5mins",
+                targetUrl: "15nnoeazZ52Q5HKZBP6c9sOgoVZD24p8Qa-HMLLQrNE4",
+                targetSheet: "PA_ALL_TEST",
+                targetStart: ""
+            },
             apiKey: "phx_apIWBmYRALmdzndWYpV1lh1X5V6hitAFKqjLtq4799DgO81",
             host: "https://us.posthog.com",
             project: "85867",
             eventId: '',
-            properties: '',
+            properties: {
+                sourceUrl: "15nnoeazZ52Q5HKZBP6c9sOgoVZD24p8Qa-HMLLQrNE4",
+                sourceSheet: "PA_ALL",
+            },
             data: "",
+            showDialog: false,
+            columnDefs: [
+                {headerName: "UUID", field: "uuid"},
+                {headerName: "Event", field: "event"},
+                {headerName: "Properties", field: "properties"},
+                {headerName: "Timestamp", field: "timestamp"},
+                {headerName: "Service", field: "service"}
+            ],
         };
     },
     methods: {
@@ -33,7 +73,10 @@ export default {
             const body = {
                 query: {
                     kind: "HogQLQuery",
-                    query: "select properties.sourceUrl AS '来源路径' from persons where properties.sourceUrl is not null",
+                    query: `select *
+                            from events
+                            where properties.sourceUrl = '${this.configData.sourceUrl}'
+                              AND properties.sourceSheet = '${this.configData.sourceSheet}'`,
                 }
             };
             const config = {
@@ -46,7 +89,7 @@ export default {
 
             try {
                 const response = await axios.post(url, body, config);
-                this.data = response.data;
+                this.data = response.data.results;
             } catch (error) {
                 console.error('Error fetching data:', error);
                 this.data = `Failed to fetch data: ${error.message}`;
@@ -60,7 +103,7 @@ export default {
             };
 
             try {
-                const response = await axios.get(url, { headers });
+                const response = await axios.get(url, {headers});
                 this.data = response.data;
             } catch (error) {
                 console.error('Failed to fetch event data:', error);
